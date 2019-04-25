6fe6fae96a1dcb1b5b04649ac014323ac04e40d5
indaam <indaam@gmail.com>
2019-04-26 02:15:52 +0700

    Update helper func

--- utils/helper.js
+++ utils/helper.js
@@ -1,5 +1,78 @@
+const fs = require('fs');
 const sh = require("shelljs");
 const HELPER = {};
+
+
+HELPER.getVersionName = (name) => {
+    if( /@/.test(name) ){
+        const data = name.split("@");
+        return {
+            name: data[0],
+            version: data[1]
+        }        
+    }
+    return {
+        name: name,
+        version: null
+    }
+}
+
+function paramsToObject(entries) {
+    let result = {}
+    for (let entry of entries) { // each 'entry' is a [key, value] tupple
+        const [key, value] = entry;
+        result[key] = value;
+    }
+    return result;
+}
+
+HELPER.createImportList = (data) => {
+    const importList = [];
+    for( let d in data){
+        const componentName = HELPER.createComponentName(d);
+        const importName = `import ${componentName} from "./${componentName}";`;
+        importList.push(importName)
+    }
+    return importList.join("\n");
+}
+
+HELPER.createsSreenList = (data) => {
+    console.log(data);
+    const screenList = [];
+    for( let d in data){
+        const componentName = HELPER.createComponentName(d);
+        screenList.push("\t" + componentName)
+    }
+    return screenList.join(",\n");
+}
+
+HELPER.stringToAbject = (str) => {
+    // iTodo, need to update
+    const obj = {};
+    if( str){
+        if (str.indexOf("=") > -1) {
+            const setArray = str.split("=");
+            obj[setArray[0]] = setArray[1];
+        } else {
+            obj[str] = str
+        }
+        return obj
+    }
+    return obj
+}
+
+HELPER.createNavigationList = () => {
+
+}
+
+HELPER.findThenUpdateContent = (data) => {
+    return data.content.replace(data.regex, function (res) {
+        if (data.updateType == "before"){
+            return data.updateContent + "\n" + res + "\n"
+        }
+        return res + "\n" + data.updateContent + "\n"
+    })
+}
 
 HELPER.setOptions = (options) => {
     if( options[1] == "comp"){
@@ -9,12 +82,67 @@ HELPER.setOptions = (options) => {
             compType : options[2],
             name : options[3]
         }        
+    } else if (options[0] == "create" ){
+        return {
+            task: options[0],
+            type: options[1],
+            name: options[2],
+        }
+    } else if (options[0] == "run"){
+        return {
+            task: options[0],
+            name: options[1],
+        }
+    
+    } else if (options[0] == "add"){
+        const versionName = HELPER.getVersionName(options[1]);
+        const otherValue = HELPER.stringToAbject(options[2]); 
+        return {
+            task: options[0],
+            name: versionName.name,
+            version: versionName.version,
+            ...otherValue
+        } 
+    }else{
+        const versionName = HELPER.getVersionName(options[1])
+        return {
+            task : options[0],
+            type : versionName.name,
+            name : HELPER.validateName(options[2]),
+            version : versionName.version
+        }        
     }
-    return {
-        task : options[0],
-        type : options[1],
-        name : options[2]
+
+}
+
+HELPER.getFileContent = (path) =>{
+    return fs.readFileSync(path, 'utf8');
+}
+
+HELPER.getFileContentFromProject = (data, filePath) => {
+    return HELPER.getFileContent(`${data.projectPath}/${filePath}`)
+}
+
+HELPER.getFileContentFromM5 = (data, filePath) => {
+    console.log("data");
+    console.log(data.dir);
+    return HELPER.getFileContent(`${data.dir.m5}/${filePath}`)
+}
+
+HELPER.getPluginInfo = (type, cmd) => {
+    if (cmd[type]){
+        return cmd[type]
     }
+    return null
+}
+
+HELPER.jsonToObject = (data) => {
+    try{
+        JSON.parse(data)
+    }catch (e){
+        throw e
+    }
+    return JSON.parse(data)
 }
 
 HELPER.validateName = (name, includeNumber) => {
@@ -84,6 +212,12 @@ HELPER.getCompType = (name) => {
         return "organisms"
     }
 }
+HELPER.validateFilePath = (data, filePath) => {
+    if( /__APP_NAME__/.test(filePath)){
+        return filePath.replace(/__APP_NAME__/, data.appJson.name);
+    }
+    return filePath
+}
 
 HELPER.getCompTargetDir = (compDir, cmd) => {
     const type = HELPER.getCompType(cmd.compType)
@@ -92,6 +226,33 @@ HELPER.getCompTargetDir = (compDir, cmd) => {
     }
     return `${compDir}/${type}/${cmd.name}`
 
+}
+
+HELPER.toCapital = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+HELPER.toArr = (str, splitBy) => {
+    return str.split(splitBy);
+}
+
+HELPER.createComponentName= (name) => {
+    const nameArr = HELPER.toArr(name, "-");
+    let nameCapital = [];
+    for (let index = 0; index < nameArr.length; index++) {
+        nameCapital.push(HELPER.toCapital(nameArr[index]))
+    }
+    return nameCapital.join("")
+}
+
+HELPER.getPluginVersion = (data) => {
+    return data.pluginVersion ? data.pluginVersion : data["version"][data.pluginName]
+}
+
+HELPER.message = (msg) => {
+    console.log("=========================");
+    console.log(msg);
+    console.log("=========================");
 }
 
 HELPER.getLastArray = (arr) => {
