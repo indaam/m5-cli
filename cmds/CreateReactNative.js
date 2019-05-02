8d1b3208fc978944e0820d1a162430c0597f21fd
indaam <indaam@gmail.com>
2019-05-02 15:43:26 +0700

    Update cmd create react native

---
+++ cmds/CreateReactNative.js
@@ -0,0 +1,116 @@
+const fs = require('fs');
+const HELPER = require('../utils/helper');
+const error = require('../utils/error');
+
+module.exports = async (cmd) => {
+
+    const DIRECTORY = {
+        current: process.cwd(),
+        m5: HELPER.getm5Path(__dirname)
+    }
+
+    class CreateReactNative {
+
+        constructor(cmd, dir) {
+            this.data = {
+                cmd: cmd,
+                dir: dir,
+                projectName: HELPER.validateName(cmd.name, true),
+                projectPath: `${dir.current}/${HELPER.validateName(cmd.name, true)}`,
+            };
+            this.run = {};
+        }
+
+        async initReactNative(data) {
+            if (!fs.existsSync(data.projectPath)) {
+                if ( data.cmd.version){
+                    return await HELPER.execAsync(`react-native init --version="${data.cmd.version}" ${data.projectName}`)
+                }else{
+                    return await HELPER.execAsync(`react-native init ${data.projectName}`)
+                }
+            } else {
+                return "exists"
+            }
+        }
+
+        async initPod(data) {
+            return await HELPER.execAsync(`cd ${data.projectPath}/ios && pod init`)
+        }
+
+        async updatePod(data) {
+            let podContent = HELPER.getFileContentFromM5(data, "files/Podfile");
+            let appJsonContent = HELPER.getFileContentFromProject(data, "app.json");
+
+            let appJsonObject = HELPER.jsonToObject(appJsonContent);
+
+            podContent = podContent.replace(/__APP_NAME__/gi, function (res) {
+                return `${appJsonObject.name}`;
+            });
+
+            return fs.writeFileSync(`${data.projectPath}/ios/Podfile`, podContent, 'utf8');
+        }
+
+        async installPod(data){
+            return await HELPER.execAsync(`cd ${data.projectPath}/ios && pod install`)
+        }
+
+        async updateGitignore(data) {
+            let gitignore = HELPER.getFileContentFromProject(data, ".gitignore");
+            if ((/ios\/Pods/).test(gitignore)){
+                console.log("Is Exist, do not create more")
+            }else{
+                gitignore = gitignore + "\nios/Pods"
+                return fs.writeFileSync(`${data.projectPath}/.gitignore`, gitignore, 'utf8');
+            }
+        }
+        
+        async updateScripts(data){
+
+            const packageJsonContent = HELPER.getFileContentFromProject(data, "package.json");
+            let packageJsonContentObj = JSON.parse(packageJsonContent);
+            
+            packageJsonContentObj["scripts"]["android"] = "react-native run-android";
+            packageJsonContentObj["scripts"]["a"] = "yarn android";
+            packageJsonContentObj["scripts"]["ios"] = "react-native run-ios";
+            packageJsonContentObj["scripts"]["i"] = "yarn ios";
+            packageJsonContentObj["scripts"]["reload"] = "adb shell input keyevent 82";
+            packageJsonContentObj["scripts"]["r"] = "yarn reload";
+            packageJsonContentObj["scripts"]["clean-watch"] = "watchman watch-del-all && rm -rf $TMPDIR/react-native-packager-cache-* && rm -rf $TMPDIR/metro-bundler-cache-*";
+            packageJsonContentObj["scripts"]["cw"] = "clean-watch";
+            packageJsonContentObj["scripts"]["clean-package"] = "rm -rf node_modules/ && npm cache verify && yarn install";
+            packageJsonContentObj["scripts"]["cp"] = "yarn clean-package";
+            packageJsonContentObj["scripts"]["pod"] = "cd ios && pod install";
+
+            fs.writeFileSync(`${data.projectPath}/package.json`, JSON.stringify(packageJsonContentObj, null, 2), 'utf8');
+
+        }
+
+        async addEditorConfig(data){
+            const editorconfigContent = HELPER.getFileContentFromM5(data, "files/.editorconfig");
+            fs.writeFileSync(`${data.projectPath}/.editorconfig`, editorconfigContent, 'utf8');
+        }
+
+        async runCmd(){
+            const { data } = this;
+            this.run["initReactNative"] = await this.initReactNative(data);
+            if (this.run["initReactNative"] != "exists"){
+                this.run["initPod"] = await this.initPod(data);
+                this.run["updatePod"] = await this.updatePod(data);
+                this.run["installPod"] = await this.installPod(data);
+                this.run["updateGitignore"] = await this.updateGitignore(data);
+                this.run["updateScripts"] = await this.updateScripts(data);
+                this.run["addEditorConfig"] = await this.addEditorConfig(data);
+            }else{
+                error(`Project ${data.projectName} is exist`, 1);
+            }
+        }
+
+        init() {
+            this.runCmd();
+        }
+    }
+
+    const runRN = new CreateReactNative(cmd, DIRECTORY);
+    runRN.init();
+
+}
