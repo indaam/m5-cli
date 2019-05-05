5ce3e35cca814db796c096a5bc2b7d0c5c0cad5c
indaam <indaam@gmail.com>
2019-05-05 13:38:24 +0700

    Update help

--- cmds/help.js
+++ cmds/help.js
@@ -6,8 +6,12 @@ const menus = {
     create react-js <name>  ............................ Create react js project (On progress)
     create react-native <name>  ........................ Create react Native project (On progress)
     create comp <comp_type> <comp_name> ................ Create react component
-    version ............................................ show package version
-    help ............................................... show help menu for a command`,
+    add <plugins_name> <key>?(optional) ................ Add react native plugins
+    demo ............................................... Create react native demo
+    install ............................................ Install all pluns from app.json
+    fonts <font_name>................................... Auto install & create font
+    version ............................................ Show package version
+    help ............................................... Show help menu for a command`,
 }
 
 module.exports = (args) => {
