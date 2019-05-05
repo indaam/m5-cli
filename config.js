44932e6d12722eb5cd31116b9b7c7f0bbdf21440
indaam <indaam@gmail.com>
2019-05-06 01:35:42 +0700

    Update config for react native demo

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const CONFIG = {
   repo : {
     html : "https://xituz@bitbucket.org/xituz/html.git",
     reactJS : null,
+    reactNativeDemo : "https://github.com/aleph-m5/m5-cli-react-native-demo",
     reactNative : null
   }
 }
