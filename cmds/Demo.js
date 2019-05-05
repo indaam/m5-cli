d566ee63d02afe9ca27ea866a0a09886908ac9bd
indaam <indaam@gmail.com>
2019-05-05 13:36:03 +0700

    Update demo for fonts

--- cmds/Demo.js
+++ cmds/Demo.js
@@ -32,10 +32,10 @@ module.exports = async (cmd) => {
         async updateRoutes(info){
             HELPER.message("RUN updateRoutes");
             const appJsonContent = HELPER.getFileContentFromProject(info, "app.json");
-            const appJsonObject = HELPER.jsonToObject(appJsonContent);
-            const { nativePlugins } = appJsonObject;
-            const importList = HELPER.createImportList(nativePlugins);
-            const screenList = HELPER.createsSreenList(nativePlugins);
+            const appJson = HELPER.jsonToObject(appJsonContent);
+            const { nativePlugins, fonts } = appJson;
+            const importList = HELPER.createImportList(nativePlugins, fonts);
+            const screenList = HELPER.createsSreenList(nativePlugins, fonts);
             let routesDemo = HELPER.getFileContentFromProject(info, `${demoDirName}/routes/index.js`);
 
             routesDemo = HELPER.findThenUpdateContent({
@@ -71,13 +71,13 @@ module.exports = async (cmd) => {
         async updateNavList(info){
             HELPER.message("RUN updateNavList");
             const appJsonContent = HELPER.getFileContentFromProject(info, "app.json");
-            const appJsonObject = HELPER.jsonToObject(appJsonContent);
-            const { nativePlugins } = appJsonObject;
-            const navList = HELPER.createNavigationList(nativePlugins);
+            const appJson = HELPER.jsonToObject(appJsonContent);
+            const { nativePlugins, fonts } = appJson;
+            const navList = HELPER.createNavigationList(nativePlugins, fonts);
             let landingDemoContent = HELPER.getFileContentFromProject(info, `${demoDirName}/containers/Landing/index.js`);
 
             landingDemoContent = HELPER.findThenUpdateContent({
-                regex : /const(\s)?navigationList(\s)?=(\s)?(\[[^\]]+\])/gi,
+                regex : /const(\s)?navigationList(\s)?=(\s)?((\[[^\]]+\])|\[\])/gi,
                 fileContent: navList,
                 originalContent: landingDemoContent,
                 updateType: "replace",
@@ -101,7 +101,7 @@ module.exports = async (cmd) => {
 
         async addReactNativeGestureHandler(info){
             const { packageJson } = info;
-            HELPER.message("RUN addReactNavigation");
+            HELPER.message("RUN addReactNativeGestureHandler");
             if( packageJson && packageJson.dependencies && !packageJson.dependencies["react-native-gesture-handler"]){
                 const cmd = {
                     task: 'add',
@@ -113,6 +113,37 @@ module.exports = async (cmd) => {
             }else{
                 return 1
             }
+        }
+
+        async createFontContainer(info, fontName, value){
+            const containerName = `Font${fontName}`;
+            const fontFamilyList = HELPER.createFontFamilyList(fontName, value);
+            const propsLists = HELPER.createPropsLists(fontName, value);
+            const textLists = HELPER.createTextLists(fontName, value);
+            const contentIndex = fontTemplate(fontFamilyList, propsLists, textLists);
+
+            const containerIsExists = fs.existsSync(`${info.projectPath}/${demoDirName}/containers/${containerName}`)
+            const indexIsExists = fs.existsSync(`${info.projectPath}/${demoDirName}/containers/${containerName}/index.js`)
+
+            if(!containerIsExists){
+                await HELPER.execAsync(`cd ${info.projectPath}/${demoDirName}/containers/ && mkdir ${containerName}`)
+            }
+
+            if(!indexIsExists){
+                await HELPER.execAsync(`cd ${info.projectPath}/${demoDirName}/containers/${containerName} && touch index.js`)
+            }
+
+            return fs.writeFileSync(`${info.projectPath}/${demoDirName}/containers/${containerName}/index.js`, contentIndex, 'utf8');
+        }
+
+        async addFontsDemo(info){
+            const appJsonContent = HELPER.getFileContentFromProject(info, "app.json");
+            const appJson = HELPER.jsonToObject(appJsonContent);
+            const { fonts } = appJson;
+            for( let f in fonts){
+                await this.createFontContainer(info, f, fonts[f]);
+            }
+            return 1
         }
 
         async addMessage(info){
@@ -128,6 +159,7 @@ module.exports = async (cmd) => {
             this.run["updateRootIndex"] = await this.updateRootIndex(info);
             this.run["updateRoutes"] = await this.updateRoutes(info);
             this.run["updateNavList"] = await this.updateNavList(info);
+            this.run["addFontsDemo"] = await this.addFontsDemo(info);
             this.run["addMessage"] = await this.addMessage(info);
         }
 
@@ -143,3 +175,55 @@ module.exports = async (cmd) => {
     runDemo.init();
 
 }
+
+
+const fontTemplate = (fontFamilyList, propsLists, textLists) => {
+const data = `
+/**
+ * This is file generated
+ * author : indaam<indaam@gmail.com>
+ */
+
+import React, { Component } from "react";
+import { View, Text as T } from 'react-native';
+import BaseContainer from '../BaseContainer';
+import MainTemplate from '../../templates/MainTemplate';
+import { COLORS } from "../../config";
+
+const fontFamily = {
+${fontFamilyList}
+}
+
+const getStyles = (props) => {
+    let fontWeight = "Regular";
+${propsLists}
+    return {
+        fontFamily: fontFamily[fontWeight]
+    }
+}
+
+const textComp = (props) => {
+    const { children, style } = props;
+    return <T {...props} style={[{ ...getStyles(props) }, style ? style : {}]}>{children}</T>
+}
+
+const Text = React.memo(textComp);
+
+export default class FontOpenSans extends BaseContainer {
+    render() {
+        return (
+            <MainTemplate padding>
+                <View>
+                ${textLists}
+                </View>
+            </MainTemplate>
+        );
+    }
+}
+`;
+return data
+}
+
+
+// iTodo
+// check when font exist
